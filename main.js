2c715fb338bea816fa56be4f3bdf3be93f11dc2c
Euang <98649953+Euang1812@users.noreply.github.com>
2022-04-17 22:56:50 +0900

    JS : Navbar menu click

--- main.js
+++ main.js
@@ -4,10 +4,31 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+const home = document.querySelector('#home');
+const about = document.querySelector('#about');
+const skill = document.querySelector('#skill');
+const work = document.querySelector('#work');
+const testimonials = document.querySelector('#testimonials');
+const contact = document.querySelector('#contact');
+
 document.addEventListener('scroll',()=>{
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     }else{
         navbar.classList.remove('navbar--dark');
     }
-})
+});
+
+//Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(e)=>{
+    const link = e.target.dataset.link;
+    if(link == null){
+        return;
+    }
+
+    console.log(e.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
