e4fa2213db2d9577c7e26d809756074187143be9
Euang <98649953+Euang1812@users.noreply.github.com>
2022-04-18 02:34:35 +0900

    JS : Arrow up

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeight) {
+
+  if (window.scrollY > (navbarHeight - 20)) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+
 });
+
 
 //Handle scrolling when tapping on the navbar menu
 
@@ -37,13 +40,25 @@ contactBtn.addEventListener('click', () => {
 
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll',()=>{
-  home.style.opacity = (1- window.scrollY / homeHeight);
+document.addEventListener('scroll', () => {
+  home.style.opacity = (1 - window.scrollY / homeHeight);
 })
 
 
+//Show "arrow up" button when scrolling down
 
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
 
+arrowUp.addEventListener('click',()=>{
+  scrollIntoView('#home');
+});
 
 //scroll function
 function scrollIntoView(selector) {
