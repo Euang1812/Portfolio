e2ee72d8a243ea5508d7dcc12844f00717699adf
Euang <98649953+Euang1812@users.noreply.github.com>
2022-04-17 23:06:26 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -4,31 +4,36 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-const home = document.querySelector('#home');
-const about = document.querySelector('#about');
-const skill = document.querySelector('#skill');
-const work = document.querySelector('#work');
-const testimonials = document.querySelector('#testimonials');
-const contact = document.querySelector('#contact');
-
-document.addEventListener('scroll',()=>{
-    if(window.scrollY > navbarHeight){
-        navbar.classList.add('navbar--dark');
-    }else{
-        navbar.classList.remove('navbar--dark');
-    }
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
 });
 
 //Handle scrolling when tapping on the navbar menu
 
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click',(e)=>{
-    const link = e.target.dataset.link;
-    if(link == null){
-        return;
-    }
+navbarMenu.addEventListener('click', (e) => {
+  const link = e.target.dataset.link;
+  if (link == null) {
+    return;
+  }
 
-    console.log(e.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+  scrollIntoView(link);
 });
+
+
+//handle click on "contact me" button on home
+const contactBtn = document.querySelector('.home__contact');
+
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+})
+
+//scroll function
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' }); Î
+}
