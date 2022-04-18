23cd23c88e74ead9b78c331e7622552eaeb6f835
Euang <98649953+Euang1812@users.noreply.github.com>
2022-04-18 23:57:45 +0900

    JS : Project filtering

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
 
   if (window.scrollY > (navbarHeight - 20)) {
@@ -25,7 +24,6 @@ navbarMenu.addEventListener('click', (e) => {
   }
   scrollIntoView(link);
 });
-
 
 //handle click on "contact me" button on home
 const about = document.querySelector("#about");
@@ -56,12 +54,40 @@ document.addEventListener('scroll', () => {
   }
 });
 
-arrowUp.addEventListener('click',()=>{
+arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
+
+
+//Projects
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(e)=>{
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null){
+    return;
+  }
+  projectContainer.classList.add('anime-out');
+
+  setTimeout(()=>{
+    projects.forEach((project)=>{
+      if(filter === '*' || filter === project.dataset.type){
+        project.classList.remove('invisble');
+      }else{
+        project.classList.add('invisble');
+      }
+    });
+    projectContainer.classList.remove('anime-out');
+  },200);
+})
 
 //scroll function
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+
