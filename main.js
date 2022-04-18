eefd6b3cfbe252bdff34de9131e796c6a2a3a954
Euang <98649953+Euang1812@users.noreply.github.com>
2022-04-19 03:15:40 +0900

    Final Touch!

--- main.js
+++ main.js
@@ -3,16 +3,21 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarName = document.querySelector('.navbar__logo__name');
 document.addEventListener('scroll', () => {
-
   if (window.scrollY > (navbarHeight - 20)) {
     navbar.classList.add('navbar--dark');
+    navbarMenu.classList.add('navbar--dark');
+    navbarName.classList.add('navbar--dark');
+
   } else {
     navbar.classList.remove('navbar--dark');
+    navbarMenu.classList.remove('navbar--dark');
+    navbarName.classList.remove('navbar--dark');
   }
 
+  navbarMenu.classList.remove('open');
 });
-
 
 //Handle scrolling when tapping on the navbar menu
 
@@ -22,8 +27,20 @@ navbarMenu.addEventListener('click', (e) => {
   if (link == null) {
     return;
   }
+
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+
+
+//navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
+
+
 
 //handle click on "contact me" button on home
 const about = document.querySelector("#about");
@@ -65,9 +82,9 @@ const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 
-workBtnContainer.addEventListener('click',(e)=>{
+workBtnContainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if(filter == null){
+  if (filter == null) {
     return;
   }
 
@@ -75,22 +92,22 @@ workBtnContainer.addEventListener('click',(e)=>{
   const active = document.querySelector('.category__btn.selected');
   active.classList.remove('selected');
 
-  const target = e.target.nodeName ==='BUTTON'? e.target : e.target.parentNode;
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
   // console.log(e.target.nodeName); nodeName은 무엇인지 확인차.!
   target.classList.add('selected');
 
   projectContainer.classList.add('anime-out');
 
-  setTimeout(()=>{
-    projects.forEach((project)=>{
-      if(filter === '*' || filter === project.dataset.type){
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
         project.classList.remove('invisble');
-      }else{
+      } else {
         project.classList.add('invisble');
       }
     });
     projectContainer.classList.remove('anime-out');
-  },200);
+  }, 200);
 })
 
 //scroll function
